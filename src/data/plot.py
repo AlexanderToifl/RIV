5ddec9c9c70b8542ff989f70de092cba4da1ab33
Christian Schleich <e1325958@student.tuwien.ac.at>
2018-04-05 17:54:01 +0200

    fitting of data

--- src/data/plot.py
+++ src/data/plot.py
@@ -3,66 +3,59 @@
 import numpy as np
 import pylab as plt
 import myPyCRV as CRV
+import math as m
+import scipy.optimize as opt
 
-n_seeds = 30
-width =	 1.			#bin width
-
-def plot_hist(outname, data, width, lo_x, hi_x, lo_y, hi_y):
-
-	mean = (np.asarray(data)).mean()
-
-	bins = np.arange(lo_x,hi_x,width)
-	histo,bins1 = np.histogram(data,bins=bins,density = False)
-	bins2 = np.delete(bins1,len(bins1)-1)
-	width_a = np.multiply(np.ones(len(bins1)-1),width)
-
-	fig = plt.figure(1)
-
-	plt.rc('text', usetex=True)  
-	plt.rc('font', family='serif') 
-	plt.rcParams.update({'font.size': 22})
-
-	plt.text(6,20, 'Mean Yield = {:.2}'.format(mean), backgroundcolor='white')
-
-	plt.axis([lo_x, hi_x , lo_y, hi_y],log=False)
-	plt.bar(bins2, histo ,width_a, align='center', alpha=0.9, color='blue',label='yield distribution')
-	plt.axvline(x=mean, ymin=0, ymax = 1, linewidth=2, color='green', label='mean')
-	plt.grid()
-	plt.xlabel('Sputteryield', fontsize=12)
-	plt.ylabel('\# of Experiments', fontsize=12)
-	plt.legend(loc='upper right', shadow=False, fontsize=12)
+def eta_exp(h,s0,Ebi,Ed,Er,eta):
+	# h ... Nr of timesteps - divide by 10000 to get recoil nr. --> calc ndpa
+	# s0 .. initial stress
+	# Ebi.. biaxial module a-Si
+	# Ed .. displace energy
+	# Er .. Recoil Energy
+	# eta.. RIV
+	ndpa = Er/(2.5*Ed)*h/10000.
+	return s0*np.exp(-Ebi/(6.*eta)*ndpa)
 	
-	print 'Mean Value of Yield: ' + str(mean)
-    
-	fig.savefig(outname)
-	plt.close("all")
-
 if __name__ == '__main__':
-	## CRV File - all seeds for all angles with 1 atom
-	crvfile = 'recoil_20_10516_1.crv'
-	outname = 'rec_50eV_20_10516_y.png'
+	## CRV File - renamed to .crv and manually changed header (crv format) from extracted data file
+	crvfile = 'recoil_40_10516_1.crv'
+	## PNG output
+	outname = 'rec_50eV_40_10516_y.png'
+	## timestep used in recoil.in during recoil insertion --> datapoint after each recoil
 	timestep = 500
-
-	crv = CRV.CRV(crvfile)[0]
-	pyy = crv['pyy']
 	
+	Er = 50.		#eV
+	Ed = 10.		#eV
+	Ebi = 150.e9	#Pa
+	
+	crv = CRV.CRV(crvfile)[0]
+	#extract mech. stress tensor in x or y
+	pyy = crv['pyy']
+	#calc timeaxis out of pxx or pyy size
 	x = np.arange(0,len(pyy)*timestep,timestep)
 	
+	#reduce timeaxis to recoil axis	(every 20 timesteps - 1 recoil)
 	x_new = []
 	pxx_new = []
 	
-	print len(x)
-	print len(pyy)
-	
-	x_new.append(x[0])
-	pxx_new.append(pyy[0])
-	
 	for i in range(len(x)/20):
 		x_new.append(x[i*20])
-		pxx_new.append(pyy	[i*20])
+		pxx_new.append(pyy[i*20])
 		
-	print len(x_new)
-	print len(pxx_new)
+	#remove first entry
+	x_new.pop()
+	pxx_new.pop()
+
+	#fit to 
+	popt, pcov = opt.curve_fit(lambda l,eta: eta_exp(l,pxx_new[0],Ebi,Ed,Er,eta) , x_new, pxx_new, p0 = [8.e12], bounds = [0.,np.inf])
+	
+	print popt[0]
+	
+	#plot fit 
+	Pfit = []
+	for i in range(len(x_new)):
+		Pfit.append(eta_exp(x_new[i],pxx_new[0],Ebi,Ed,Er,popt[0]))
+	
 
 	fig = plt.figure(1)
 
@@ -72,9 +65,10 @@ if __name__ == '__main__':
 
 	#plt.text(6,20, 'Mean Yield = {:.2}'.format(mean), backgroundcolor='white')
 
-	plt.axis([0, 200000 , 0, 0.3],log=False)
+	plt.axis([0, 300000 , 0, 0.3],log=False)
 	plt.grid()
 	plt.plot(x_new,np.abs(pxx_new),'b-')
+	plt.plot(x_new,np.abs(Pfit),'r-')
 	plt.xlabel('Timestep', fontsize=12)
 	plt.ylabel('Pyy', fontsize=12)
 	plt.legend(loc='upper right', shadow=False, fontsize=12)
